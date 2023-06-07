d9cfe27a9ef07a3bb6a3a6e1f6521844b680b251
ScyBry <99564441+ScyBry@users.noreply.github.com>
2023-06-07 15:03:08 +0300

    Making navbar

---
+++ src/components/Call/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.root}>
+      <span>+7 7172 264 55 55</span>
+    </div>
+  );
+}
