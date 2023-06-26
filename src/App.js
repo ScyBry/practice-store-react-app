e5adf0b64d18414a6983da8610f1afb3019b13af
ScyBry <99564441+ScyBry@users.noreply.github.com>
2023-06-26 09:11:40 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
-
-import { Home, Card, CartPage } from './pages';
-import { ControlledOpenSpeedDial } from './components';
+import { useDispatch, useSelector } from 'react-redux';
+import { Home, Card, CartPage, CatalogPage } from './pages';
+import { ControlledOpenSpeedDial, Popup } from './components';
 import { Navbar, Footer } from './components';
+import { setPopup } from './redux/slices/popupSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const open = useSelector((state) => state.popup.open);
+
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <ControlledOpenSpeedDial></ControlledOpenSpeedDial>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/card/:id" element={<Card></Card>}></Route>
-        <Route path="/cart" element={<CartPage></CartPage>}></Route>
-      </Routes>
-      <Footer />
+      <div className="content">
+        <Navbar></Navbar>
+        <ControlledOpenSpeedDial></ControlledOpenSpeedDial>
+        <Popup dispatch={dispatch} open={open} setPopup={setPopup}></Popup>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/card/:id" element={<Card></Card>}></Route>
+          <Route path="/cart" element={<CartPage></CartPage>}></Route>
+          <Route path="/catalog" element={<CatalogPage></CatalogPage>}></Route>
+        </Routes>
+        <Footer />
+      </div>
     </div>
   );
 }
